577a46f1be8783eec8b35c6ad1ff15ac23d88152
Stephanie <stephogam@gmail.com>
2021-04-18 23:50:01 +0000

    Create css file for styling content

--- static/js/profile.js
+++ static/js/profile.js
@@ -20,39 +20,80 @@
 
 // form.html
 
-$(function(){
-  $("form[id='createprofile']").validate({
-    rules: {
-      fname: "required",
-      lname: "required",
-      email: {
-        required: true,
-        email: true
-      },
-      password: {
-        required: true,
-        minlength: 7
-      },
-      phone: {
-        required: true,
-        minlength: 10
+$(document).ready(function($) {
+        
+  $("#register-form").validate({
+          rules: {
+              name: "required",                    
+              password: {
+                  required: true,
+                  minlength: 6
+              },
+             city: "required",
+            gender: "required"
+           
+          },
+          messages: {
+              fname: "Please enter your first name",
+              lname: "Please enter your last name",
+              email: "Please enter a valid email",                   
+              password: {
+                  required: "Please provide a password",
+                  minlength: "Your password must be at least 6 characters long"
+              },
+            gender: "This field is required"
+          },
+           errorPlacement: function(error, element) 
+  {
+      if ( element.is(":radio") ) 
+      {
+          error.appendTo( element.parents('.form-group') );
       }
-    },
-    messages: {
-      fname: "Please enter your firstname",
-      lname: "Please enter your lastname",
-      password: {
-        required: "Please provide a password",
-        minlength: "Your password must be at least 7 characters long"
-      },
-      email: "Please enter a valid email address" 
-    },
-      phone: {
-        required: "Please provide your phone number",
-        minlength: "Your phone number must be 10 digits long"
-      },
-  submitHandler: function(form) {
-    form.submit();
-  }
- });
+      else 
+      { // This is the default behavior 
+          error.insertAfter( element );
+      }
+   },
+          submitHandler: function(form) {
+              form.submit();
+          }
+          
+      });
 });
+
+// $(function(){
+//   $("#createprofile").validate({
+//     rules: {
+//       fname: "required",
+//       lname: "required",
+//       email: {
+//         required: true,
+//         email: true
+//       },
+//       password: {
+//         required: true,
+//         minlength: 7
+//       },
+//       phone: {
+//         required: true,
+//         minlength: 10
+//       }
+//     },
+//     messages: {
+//       fname: "Please enter your firstname",
+//       lname: "Please enter your lastname",
+//       password: {
+//         required: "Please provide a password",
+//         minlength: "Your password must be at least 7 characters long"
+//       },
+//       email: "Please enter a valid email address" 
+//     },
+//       phone: {
+//         required: "Please provide your phone number",
+//         minlength: "Your phone number must be 10 digits long"
+//       },
+//   submitHandler: function(form) {
+//     form.submit();
+//   }
+//  });
+// });
