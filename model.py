d6214eff5d8d9f45d4d85761d1caf952ce253bf3
Stephanie <stephogam@gmail.com>
2021-05-01 20:36:45 +0000

    Create login form. Fix password hash error

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ class User(db.Model):
     fname = db.Column(db.String(100), nullable=False)
     lname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True, index=True)
-    # password = db.Column(db.String(100), nullable=False)
+    # password = db.Column(db.String(100))
     password_hash = db.Column(db.String(100))
     phone = db.Column(db.String, nullable=False)
 
