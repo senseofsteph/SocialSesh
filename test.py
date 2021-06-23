1f424003d445610a3a9fc82f68ebc5f02dd87f5c
Stephanie <stephogam@gmail.com>
2021-06-23 19:25:23 +0000

    Add unittest for calendar route; fix unittest for all events route

--- test.py
+++ test.py
@@ -13,38 +13,58 @@ class MyAppIntegrationTestCase(unittest.TestCase):
       self.client = server.app.test_client()
       server.app.config['TESTING'] = True
 
+
     def tearDown(self):
       """Function to complete after each test."""
+
 
     def test_index(self):
         client = server.app.test_client()
         result = client.get('/')
         self.assertIn(b'<div id="header" class="header">', result.data)
 
+
     def test_form(self):
         client = server.app.test_client()
         result = client.get('/form')
         self.assertIn(b'<div id="form-signup" class="form-signup">', result.data)
 
+
     def test_login(self):
         client = server.app.test_client()
         result = client.get('/login')
         self.assertIn(b'<div id="form-login" class="form-login">', result.data)
+
     
     def test_profile(self):
         client = server.app.test_client()
         result = client.get('/profile')
         self.assertIn(b'<div id="profile-header" class="profile-header">', result.data)
 
+
+    def test_calendar(self):
+        client = server.app.test_client()
+        result = client.get('/calendar')
+        self.assertIn(b'<title>Full Calendar of Events</title>', result.data)
+
+
     def test_event_details(self):
         client = server.app.test_client()
         result = client.get('/category')
         self.assertIn(b'<div id="event-category" class="event-category">', result.data)
+
+    
+    # def test_all_events(self):
+    #     client = server.app.test_client()
+    #     result = client.get('/events')
+    #     self.asserIn(b'<div id="all-events" class="all-events">', result.data)
+
     
     def test_register(self):
         client = server.app.test_client()
         result = client.get('/register')
         self.assertIn(b'<div id="form-register" class="form-register">', result.data)
+
 
     def test_confirm(self):
         client = server.app.test_client()
@@ -52,6 +72,7 @@ class MyAppIntegrationTestCase(unittest.TestCase):
         self.assertIn(b'<div id="confirmation-header" class="confirmation-header">', result.data)
 
     
+
 
 #**** ------------------------------- ****#
 
