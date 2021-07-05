17e1f69bd678f1bc9147b4f924593c8b67451484
Stephanie <stephogam@gmail.com>
2021-07-05 19:37:59 +0000

    Left comment to pass in user name when logged in to profile; update flash message

--- server.py
+++ server.py
@@ -82,6 +82,7 @@ def login_user():
    
     if crud.is_email_and_password_valid(email, password):
         session['user_id'] = email
+        # pass in user first name to pass into flash
         flash("You're logged in.")
         return redirect("/profile")
     else:
