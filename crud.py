2d586ce6492e43ed2d43c4d4e7aa22c52086715d
Stephanie <stephogam@gmail.com>
2021-07-11 00:13:40 +0000

    Add query to fetch user by first name

--- crud.py
+++ crud.py
@@ -40,6 +40,12 @@ def get_user_by_id(user_id):
     return User.query.get(user_id)
 
 
+def get_user_by_fname(fname):
+    """Return a user by first name."""
+
+    return User.query.filter(User.fname)
+
+
 def get_user_by_email(email):
     """Return a user by email."""
 
