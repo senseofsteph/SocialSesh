579c6bc2bf5219520875590bc40bb82774546352
Stephanie <stephogam@gmail.com>
2021-08-03 22:04:36 +0000

    Set default image for user profile; Temporary

--- model.py
+++ model.py
@@ -39,7 +39,7 @@ class User(db.Model):
     email = db.Column(db.String(100), nullable=False, unique=True, index=True)
     password_hash = db.Column(db.String(100))
     phone = db.Column(db.String, nullable=False)
-    image = db.Column(db.Text)
+    image = db.Column(db.String, nullable=False, default='default-profile-picture.png')
 
     # users_events = a list of User_Event objects
     
